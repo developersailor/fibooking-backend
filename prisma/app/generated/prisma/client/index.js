65b061c60bf34e25340bf9c2b7ef37e32dc4d954
mehmet fiskindal <developersailor@gmail.com>
2025-04-21 12:43:35 +0300

    chore: Update Prisma configuration to remove hardcoded database URL and add postinstall flag

--- prisma/app/generated/prisma/client/index.js
+++ prisma/app/generated/prisma/client/index.js
@@ -471,11 +471,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://postgres:rootme17&@localhost:5432/fibooking"
+        "value": null
       }
     }
   },
